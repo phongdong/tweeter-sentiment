313f7522f434c5b36c1b35f5072d6235916703fd
phongdong <phongdong@gmail.com>
2017-05-29 20:05:54 -0700

    testing

--- hello.js
+++ hello.js
@@ -8,7 +8,7 @@ var destroy = require('destroy');
 // make Stream globally visible so we can clean up better
 var stream;
 
-var DEFAULT_TOPIC = "Bieber";
+var DEFAULT_TOPIC = "testing Bieber";
 
 // defensiveness against errors parsing request bodies...
 process.on('uncaughtException', function (err) {
